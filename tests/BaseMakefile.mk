db6fa480003b4af9cadc6518a4d3d3d9a2dab65e
Yuechao Pan <ychpan@ucdavis.edu>
2017-06-13 13:44:35 -0700

    new branch for working with Open MPI; updated BaseMakefile.mk

--- tests/BaseMakefile.mk
+++ tests/BaseMakefile.mk
@@ -11,6 +11,7 @@
 
 force64 = 1
 use_metis = 1
+use_boost = 1
 NVCC = "$(shell which nvcc)"
 NVCC_VERSION = $(strip $(shell nvcc --version | grep release | sed 's/.*release //' |  sed 's/,.*//'))
 
@@ -31,7 +32,7 @@ GEN_SM37 = -gencode=arch=compute_37,code=\"sm_37,compute_37\"
 GEN_SM35 = -gencode=arch=compute_35,code=\"sm_35,compute_35\"
 GEN_SM30 = -gencode=arch=compute_30,code=\"sm_30,compute_30\"
 
-SM_TARGETS = $(GEN_SM35) #$(GEN_SM35) $(GEN_SM60) $(GEN_SM61) 
+SM_TARGETS = $(GEN_SM30) $(GEN_SM35) #$(GEN_SM60) $(GEN_SM61)
 #-------------------------------------------------------------------------------
 # Libs
 #-------------------------------------------------------------------------------
@@ -41,19 +42,37 @@ SM_TARGETS = $(GEN_SM35) #$(GEN_SM35) $(GEN_SM60) $(GEN_SM61)
 # Includes
 #-------------------------------------------------------------------------------
 
-CUDA_INC = "$(shell dirname $(NVCC))/../include"
-MGPU_INC = "../../externals/moderngpu/include"
-CUB_INC = "../../externals/cub"
-BOOST_DEPS = -I../../.. -Xlinker -lboost_system -Xlinker -lboost_chrono -Xlinker -lboost_timer -Xlinker -lboost_filesystem
-OMP_DEPS = -Xcompiler -fopenmp -Xlinker -lgomp
+CUDA_INC = -I"$(shell dirname $(NVCC))/../include"
+MGPU_INC = -I"../../externals/moderngpu/include"
+CUB_INC = -I"../../externals/cub"
+
+BOOST_INC =
+BOOST_LINK =
+ifeq ($(use_boost), 1)
+    BOOST_INC = -I"/usr/local/include"
+    BOOST_LINK = -Xcompiler -DBOOST_FOUND -L"/usr/local/lib" -Xlinker -lboost_system -Xlinker -lboost_chrono -Xlinker -lboost_timer -Xlinker -lboost_filesystem
+endif
+
+ifeq (DARWIN, $(findstring DARWIN, $(OSUPPER)))
+    OMP_INC  = -I"/usr/local/include/libiomp"
+    OMP_LINK = -Xlinker /usr/local/lib/libiomp5.dylib
+else
+    OMP_INC =
+    OMP_LINK = -Xcompiler -fopenmp -Xlinker -lgomp
+endif
+
+ifeq (DARWIN, $(findstring DARWIN, $(OSUPPER)))
+    use_metis = 0
+endif
 
 ifneq ($(use_metis), 1)
-	METIS_DEPS =
+	METIS_LINK =
 else
-	METIS_DEPS = -Xlinker -lmetis -Xcompiler -DMETIS_FOUND
+	METIS_LINK = -Xlinker -lmetis -Xcompiler -DMETIS_FOUND
 endif
 GUNROCK_DEF = -Xcompiler -DGUNROCKVERSION=0.4.0
-INC = -I$(CUDA_INC) -I$(MGPU_INC) -I$(CUB_INC) $(BOOST_DEPS) $(OMP_DEPS) $(METIS_DEPS) $(GUNROCK_DEF) -I.. -I../..
+LINK = $(BOOST_LINK) $(OMP_LINK) $(METIS_LINK) $(GUNROCK_DEF)
+INC = $(CUDA_INC) $(OMP_INC) $(MGPU_INC) $(CUB_INC) $(BOOST_INC) -I.. -I../.. $(LINK)
 
 #-------------------------------------------------------------------------------
 # Defines
@@ -74,7 +93,7 @@ else
 	ARCH = -m64
 endif
 
-NVCCFLAGS = -Xptxas -v -Xcudafe -\# -lineinfo --std=c++11 #-ccbin=g++-4.8
+NVCCFLAGS = -Xptxas -v -Xcudafe -\# -lineinfo --std=c++11 --expt-extended-lambda #-ccbin=g++-4.8
 
 ifeq (WIN_NT, $(findstring WIN_NT, $(OSUPPER)))
 	NVCCFLAGS += -Xcompiler /bigobj -Xcompiler /Zm500
@@ -96,8 +115,20 @@ endif
 #-------------------------------------------------------------------------------
 # Dependency Lists
 #-------------------------------------------------------------------------------
-EXTRA_SOURCE = ../../gunrock/util/types.cu ../../gunrock/util/test_utils.cu ../../gunrock/util/error_utils.cu ../../externals/moderngpu/src/mgpucontext.cu ../../externals/moderngpu/src/mgpuutil.cpp ../../gunrock/util/gitsha1.c
-      
+EXTRA_SOURCE_ = \
+	../../gunrock/util/test_utils.cu \
+	../../gunrock/util/error_utils.cu \
+	../../externals/moderngpu/src/mgpucontext.cu \
+	../../externals/moderngpu/src/mgpuutil.cpp \
+	../../gunrock/util/gitsha1.c
+
+ifeq (DARWIN, $(findstring DARWIN, $(OSUPPER)))
+    EXTRA_SOURCE = $(EXTRA_SOURCE_) \
+	    ../../gunrock/util/misc_utils.cu
+else
+    EXTRA_SOURCE = $(EXTRA_SOURCE_)
+endif
+
 DEPS = 	./Makefile \
     ../BaseMakefile.mk \
     $(EXTRA_SOURCE) \
@@ -105,11 +136,13 @@ DEPS = 	./Makefile \
     $(wildcard ../../gunrock/util/**/*.cuh) \
     $(wildcard ../../gunrock/util/*.c) \
     $(wildcard ../../gunrock/*.cuh) \
+    $(wildcard ../../gunrock/graph/*.cuh) \
     $(wildcard ../../gunrock/graphio/*.cuh) \
     $(wildcard ../../gunrock/oprtr/*.cuh) \
     $(wildcard ../../gunrock/oprtr/**/*.cuh) \
     $(wildcard ../../gunrock/app/*.cuh) \
-    $(wildcard ../../gunrock/app/**/*.cuh)
+    $(wildcard ../../gunrock/app/**/*.cuh) \
+    $(wildcard ../../gunrock/partitioner/*.cuh)
 
 #-------------------------------------------------------------------------------
 # (make test) Test driver for
